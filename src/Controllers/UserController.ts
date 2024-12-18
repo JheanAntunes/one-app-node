9107e2350bca6d7fc700a6dc6277ea04683068da
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-12-18 14:15:14 -0300

    feat: add class UserController

---
+++ src/Controllers/UserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { UserService } from "../Services/UserService";
+
+export class UserController {
+  createUser = (req: Request, res: Response) => {
+    const body = req.body;
+    const userService: UserService = new UserService();
+    userService.createUser(body);
+    res.status(201).json({ message: "usuario criado." });
+  };
+
+  getAllUsers = (req: Request, res: Response) => {
+    const userService: UserService = new UserService();
+    const users = userService.getAllUsers();
+    return res.status(200).json({ message: "OK", users });
+  };
+}
